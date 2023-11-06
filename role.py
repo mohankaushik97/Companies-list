c7bf9709874862dfc76f2dc01d82b97e4062c892
mohankaushik97 <41466343+mohankaushik97@users.noreply.github.com>
2023-11-05 21:25:56 -0700

    GUI ready with Pyqt - Need to do validation

--- role.py
+++ role.py
@@ -1,20 +1,26 @@
 class Role:
-    def __init__(self,title,location,department,
-                 job_level,job_module,poc,
-                 contact_origin,contact_details,referral_contact,
-                 application_date, application_status) -> None:
+
+    def __init__(self,title="",city="",state="",department="",
+                 job_level="",job_module="",poc="",
+                 contact_origin="",contact_details="",referral_contact="",
+                 application_date="", application_status=""):
         
         self.title = title
-        self.location = location
+        self.city = city
+        self.state = state
         self.department = department
         self.job_level = job_level
         self.job_module = job_module
         self.poc = poc
-        self.contact_origin = contact_origin
-        self.contact_details = contact_details
+        self.poc_origin = contact_origin
+        self.poc_details = contact_details
         self.referral_contact = referral_contact
         self.app_date = application_date
         self.app_status = application_status        
+
+    @property
+    def location(self):
+        return '{}, {}'.format(self.city, self.state)
 
     def __repr__(self) -> str:
         return "Role('{}','{}','{}')".format(self.title,self.app_date,self.app_status)
